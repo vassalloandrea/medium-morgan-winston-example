fbde661cab22b65fdb512d2345146cca77f63894
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2021-01-09 09:31:58 +0100

    Install and configure winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import express from "express";
 
+import Logger from "./lib/logger";
+
 const app = express();
 const PORT = 3000;
 
 app.get("/logger", (_, res) => {
-  console.log("This is an error log");
-  console.log("This is a warn log");
-  console.log("This is a info log");
-  console.log("This is a http log");
-  console.log("This is a debug log");
+  Logger.error("This is an error log");
+  Logger.warn("This is a warn log");
+  Logger.info("This is a info log");
+  Logger.http("This is a http log");
+  Logger.debug("This is a debug log");
 
   res.send("Hello world");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running @ http://localhost:${PORT}`);
+  Logger.debug(`Server is up and running @ http://localhost:${PORT}`);
 });
