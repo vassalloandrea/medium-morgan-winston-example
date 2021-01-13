b35584f88cc2257c5b274a464e4bfa76a3d4da33
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2021-01-13 07:13:15 +0100

    Allow to test the logger custom library

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+}
---
+++ src/lib/logger.test.ts
@@ -0,0 +1,93 @@
+const logger = {
+  info: jest.fn(),
+  error: jest.fn(),
+}
+
+import winstonInstance from 'winston'
+
+let formatReturnValue: Function
+
+jest.mock('winston', () => ({
+  addColors: jest.fn(),
+  format: {
+    combine: jest.fn(),
+    timestamp: jest.fn(),
+    colorize: jest.fn(),
+    printf: jest.fn().mockImplementation((info) => {
+      formatReturnValue = info
+    }),
+  },
+  createLogger: jest.fn().mockReturnValue({
+    info: jest.fn(),
+  }),
+  transports: {
+    Console: jest.fn(),
+    File: jest.fn(),
+  },
+}))
+
+let winston: typeof winstonInstance
+
+describe('Logger', () => {
+  beforeEach(() => {
+    jest.resetModules()
+    winston = require('winston')
+  })
+
+  it('calls the format', () => {
+    const info = {
+      timestamp: 123,
+      level: 'info',
+      message: 'This is a message',
+    }
+
+    const logger = require('./logger').default
+    logger.info('Testing the logger')
+
+    expect(formatReturnValue(info)).toBe(
+      `${info.timestamp} ${info.level}: ${info.message}`,
+    )
+  })
+
+  describe('when the NODE_ENV is not development', () => {
+    it('creates the logger with the warn level', () => {
+      process.env.NODE_ENV = 'production'
+
+      const createLoggerMock = jest.spyOn(winston, 'createLogger')
+      const logger = require('./logger').default
+      logger.info('Testing the logger')
+
+      expect(createLoggerMock).toHaveBeenCalledWith(
+        expect.objectContaining({ level: 'warn' }),
+      )
+    })
+  })
+
+  describe('when the NODE_ENV is development', () => {
+    it('creates the logger with the debug level', () => {
+      process.env.NODE_ENV = 'development'
+
+      const createLoggerMock = jest.spyOn(winston, 'createLogger')
+      const logger = require('./logger').default
+      logger.info('Testing the logger')
+
+      expect(createLoggerMock).toHaveBeenCalledWith(
+        expect.objectContaining({ level: 'debug' }),
+      )
+    })
+  })
+
+  describe('when the NODE_ENV is not set', () => {
+    it('creates the logger with the debug level', () => {
+      process.env.NODE_ENV = ''
+
+      const createLoggerMock = jest.spyOn(winston, 'createLogger')
+      const logger = require('./logger').default
+      logger.info('Testing the logger')
+
+      expect(createLoggerMock).toHaveBeenCalledWith(
+        expect.objectContaining({ level: 'debug' }),
+      )
+    })
+  })
+})
